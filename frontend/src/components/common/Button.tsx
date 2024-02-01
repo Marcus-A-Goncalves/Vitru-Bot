e98b10aca13e42e5b3818b692c19fc5ed511049e
Marcus V. A. Gonçalves <marcus.goncalves@bizapp.com.br>
2024-02-01 12:16:52 -0300

    Commit inicial, clone azure repos

---
+++ frontend/src/components/common/Button.tsx
@@ -0,0 +1,71 @@
+import { CommandBarButton, DefaultButton, IButtonProps, IButtonStyles, ICommandBarStyles } from "@fluentui/react";
+
+interface ShareButtonProps extends IButtonProps {
+    onClick: () => void;
+  }
+
+export const ShareButton: React.FC<ShareButtonProps> = ({onClick}) => {
+    const shareButtonStyles: ICommandBarStyles & IButtonStyles = {
+        root: {
+          width: 'fit-content',
+          height: 32,
+          borderRadius: 4,
+          background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #FFDD00 33.63%, #EBC300 100%)',
+        //   position: 'absolute',
+        //   right: 20,
+          padding: '5px 12px',
+          marginRight: '20px',
+          transition: '1s ease',
+        },
+        icon: {
+          color: '#000',
+        },
+        rootHovered: {
+          background: 'linear-gradient(135deg, #EBC300 0%, #FFDD00 100%)',
+        },
+        label: {
+          fontWeight: 600,
+          fontSize: 14,
+          lineHeight: '20px',
+          color: '#000',
+        },
+      };
+
+      return (
+        <CommandBarButton
+                styles={shareButtonStyles}
+                iconProps={{ iconName: 'Share' }}
+                onClick={onClick}
+                text="Compartilhar"
+        />
+      )
+}
+
+interface HistoryButtonProps extends IButtonProps {
+    onClick: () => void;
+    text: string;
+  }
+
+export const HistoryButton: React.FC<HistoryButtonProps> = ({onClick, text}) => {
+    const historyButtonStyles: ICommandBarStyles & IButtonStyles = {
+        root: {
+            width: '180px',
+            border: `1px solid #D1D1D1`,
+          },
+          rootHovered: {
+            border: `1px solid #D1D1D1`,
+          },
+          rootPressed: {
+            border: `1px solid #D1D1D1`,
+          },
+      };
+
+      return (
+        <DefaultButton
+            text={text}
+            iconProps={{ iconName: 'History' }}
+            onClick={onClick}
+            styles={historyButtonStyles}
+        />
+      )
+}
