eb1639151c1ab6ce0d3483410e64bffcc24c5aca
Marcus V. A. Gonçalves <marcus.goncalves@bizapp.com.br>
2024-02-15 11:47:21 -0300

    Alteração do vitru-bot para persona Sofia

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash-es";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Robot.svg";
+import Contoso from "../../assets/sofia.png";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
